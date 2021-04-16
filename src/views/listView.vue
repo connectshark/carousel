83ef7ba78ce68ba88500dd2c2a266644979eacad
connectshark <bobhus394@gmail.com>
2021-04-16 16:28:59 +0800

    輪播重新理解

--- src/views/listView.vue
+++ src/views/listView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="list-view">
     <transition-group tag="ul" class="container" name="flip-list">
-      <li v-for="img in showImages" :key="img.key" class="list-item">
+      <li v-for="img in settingImages" :key="img.key" class="list-item">
         <figure class="img-place">
           <img :src="img.url + img.key" alt="fake img">
         </figure>
       </li>
     </transition-group>
     <nav>
-      <input type="button" value="<" @click="change(-1)">
-      <input type="button" value=">" @click="change(1)">
+      <input type="button" value="<" @click="change(show += -1)">
+      <input type="button" value=">" @click="change(show += 1)">
     </nav>
   </div>
 </template>
@@ -45,33 +45,34 @@ export default {
     }
   },
   methods: {
-    change (key) {
-      const value = this.show + key
-      const limit = this.allImages.length - 1
-      this.show = value < 0 ? limit : value > limit ? 0 : value
+    change (value) {
+      if (value > this.allImages.length - 1) {
+        this.show = 0
+      } else if (value < 0) {
+        this.show = this.allImages.length - 1
+      } else {
+        this.show = value
+      }
     }
   },
   computed: {
     allImages () {
       const arr = []
-      const total = this.imgs.length
-      let count
-      if (total > 0) {
-        while (arr.length < 5 + 4) {
-          count = Math.floor(arr.length / total)
-          this.imgs.forEach(img => {
-            arr.push({
-              url: img.url,
-              key: count + '-' + img.key
-            })
+      let count = 0
+      while (arr.length < 5 + 4) {
+        this.imgs.forEach(img => {
+          arr.push({
+            url: img.url,
+            key: count + '-' + img.key
           })
-        }
+        })
+        count++
       }
       return arr
     },
-    showImages () {
-      const start = this.show - 4
-      return this.allImages.slice(start).concat(this.allImages.slice(0, start))
+    settingImages () {
+      const startIndex = this.show - 4
+      return this.allImages.slice(startIndex).concat(this.allImages.slice(0, startIndex))
     }
   }
 }
@@ -83,31 +84,25 @@ export default {
 }
 .list-view{
   overflow: hidden;
-  width: 100%;
   .container{
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
     justify-content: center;
-    margin-left: 20%;
+    margin-left: 22%;
     .list-item{
-      width: 25%;
-      margin: 0 2%;
+      margin: 0 10px;
       flex-shrink: 0;
+      width: 25%;
+      &:first-child, &:last-child{
+        visibility: hidden;
+      }
       .img-place{
         width: 100%;
         img{
-          vertical-align: middle;
           width: 100%;
+          vertical-align: middle;
         }
-      }
-      &:first-child{
-        visibility: hidden;
-        z-index: -1;
-      }
-      &:last-child{
-        visibility: hidden;
-        z-index: -1;
       }
     }
   }
